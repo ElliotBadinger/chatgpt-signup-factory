c88f9c614c073dd0323ca8e000cc7b285aac8ea1
Elliotbadinger <brainstein@tutanota.com>
2026-02-03 08:44:38 +0200

    feat: add run config with defaults and overrides

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 export default {
   transform: {},
   testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/chrome-devtools-mcp/'],
+  testTimeout: 300000,
 };
